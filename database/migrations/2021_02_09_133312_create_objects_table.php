7cfabf2944954ce98e8d19c8d3e7f2cae236ad89
Zaw Myo Latt <zawmyolatt.ucsy@gmail.com>
2021-02-10 10:03:35 +0800

    Update 2021_02_09_133312_create_objects_table.php

--- database/migrations/2021_02_09_133312_create_objects_table.php
+++ database/migrations/2021_02_09_133312_create_objects_table.php
@@ -18,8 +18,8 @@ class CreateObjectsTable extends Migration
             $table->id();
             $table->string('key');
             $table->longText('value');
-            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+            $table->timestamp('created_at')->nullable();
         });
     }
 
