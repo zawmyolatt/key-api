f1d4b5bc521cb642c5ed0764516a45db59c98ab6
Zaw Myo Latt <zawmyolatt.ucsy@gmail.com>
2021-02-10 10:16:00 +0800

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,5 +28,8 @@ class AppServiceProvider extends ServiceProvider
     {
         //fix database scheme
         Schema::defaultStringLength(191);
+        if (App::environment() === "production") {
+            URL::forceScheme("https");
+        }
     }
 }
